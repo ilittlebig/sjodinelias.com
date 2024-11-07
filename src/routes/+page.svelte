47793b0599ee2c1868d058616947346ba175c949
Elias Sjödin <elias@amsystem.com>
2024-11-07 22:17:55 +0100

    add experience text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,5 +6,13 @@
 <Navbar />
 <HeroSection />
 
-<div class="h-screen bg-blue">
+<div class="flex justify-center h-screen">
+  <div class="relative h-fit">
+    <p class="font-playfair italic font-bold text-[150px] opacity-10 text-gradient leading-none">
+      Experience
+    </p>
+    <div class="text-2xl text-grayish absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      Previously worked at
+    </div>
+  </div>
 </div>

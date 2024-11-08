82fe36381a11409a8dde542212f2dad55f582060
Elias Sjödin <elias@amsystem.com>
2024-11-08 23:01:24 +0100

    add glow background & animations to hero section

--- src/lib/components/sections/hero-section.svelte
+++ src/lib/components/sections/hero-section.svelte
@@ -1,28 +1,84 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+  import { gsap } from "gsap";
+
+  let currentImageIndex = 0;
+  let images = [
+    "/high-performance.svg",
+    "/future-proof.svg",
+  ];
+
+  onMount(() => {
+    const timeline = gsap.timeline({ repeat: -1 });
+
+    gsap.from(".animate", {
+      opacity: 0,
+      y: 30,
+      duration: 1,
+      stagger: 0.3,
+      ease: "power2.out",
+    });
+
+    images.forEach((image, index) => {
+      timeline.fromTo(
+        `#image-${index}`,
+        {
+          y: 40,
+          opacity: 0,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 0.5,
+          ease: "power2.out",
+        }
+      )
+      .to(
+        `#image-${index}`,
+        {
+          delay: 2,
+          y: -30,
+          opacity: 0,
+          duration: 0.5,
+          ease: "power2.in",
+        },
+        "+=0.5"
+      );
+    });
+  });
 </script>
 
 <div class="flex items-center justify-center h-screen">
-  <div class="flex flex-col gap-y-12 items-center">
+  <div class="flex flex-col gap-y-12 items-center animate">
     <p class="text-subtext text-2xl text-center">
-      Hello! I’m <span class="text-grayish">Elias Sjödin</span>, <br/>
+      Hello! I’m <span class="text-grayish">Elias Sjödin</span>,
+      <br/>
       front-end developer based in Sweden.
     </p>
-    <p class="text-grayish text-5xl text-center font-medium">
-      Building seamless, accessible <br/>
+    <p class="text-5xl text-center font-semibold bg-gradient-to-b from-grayish to-[#958B8C] bg-clip-text text-transparent">
+      Building seamless, accessible
+      <br/>
       interfaces that bring ideas to life.
     </p>
-    <img src="/high-performance.svg" width={767} height={46} />
+
+    <div class="flex items-center justify-center relative w-full h-[46px]">
+      {#each images as image, index}
+        <img src={image} id={"image-" + index} width={767} height={46} class="absolute top-1/2 transform -translate-y-1/2 opacity-0" />
+      {/each}
+    </div>
 
     <div class="w-full h-px bg-gradient-to-r from-transparent via-blue to-transparent" />
 
     <div class="flex gap-x-4 items-center">
-      <p class="text-subtext text-2xl">Currently</p>
-      <p class="text-subtext text-2xl">@</p>
+      <p class="text-subtext text-xl">Currently</p>
+      <p class="text-subtext text-xl">@</p>
       <img src="/logos/microsoft-logo.svg" alt="Microsoft Logo" class="h-6" />
-      <p class="text-subtext text-2xl">|</p>
-      <p class="text-subtext text-2xl">Previously</p>
-      <p class="text-subtext text-2xl">@</p>
+      <p class="text-subtext text-xl">|</p>
+      <p class="text-subtext text-xl">Previously</p>
+      <p class="text-subtext text-xl">@</p>
       <img src="/logos/roblox-logo.svg" alt="Roblox Logo" class="h-6" />
     </div>
   </div>
+
+  <img src="/glow.png" alt="Glow Effect" width={1100} height={1100} class="absolute select-none -z-10 scale-125" />
 </div>

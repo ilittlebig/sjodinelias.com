71a5078768920b733f6dc324c902e4ae5c4c9d20
Elias Sjödin <elias@amsystem.com>
2024-11-11 00:41:02 +0100

    add personal portfolio project & fixed responsive chips

--- src/lib/components/chip.svelte
+++ src/lib/components/chip.svelte
@@ -15,11 +15,11 @@
 </script>
 
 <div class={cn(
-  "flex items-center gap-x-2 w-fit px-3 py-2 rounded-full",
+  "flex items-center gap-x-2 w-fit h-fit px-3 py-2 rounded-full",
    variant === "outline" ? "border border-dark-border" : (color || "bg-blue")
 )}>
   {#if variant === "filled"}
     <div class="w-2 h-2 shrink-0 rounded-full bg-grayish" />
   {/if}
-  <p class="text-grayish text-xs md:text-sm">{label}</p>
+  <p class="text-nowrap text-grayish text-xs md:text-sm">{label}</p>
 </div>
--- src/lib/components/project-card.svelte
+++ src/lib/components/project-card.svelte
@@ -12,7 +12,7 @@
 
 <a href="/" class="flex flex-col gap-y-4 w-full expand-on-hover px-3 md:px-0">
   <img src={image} class="bg-white rounded-card w-full h-[300px] md:h-[348px] object-cover" />
-  <div class="flex gap-x-5">
+  <div class="flex flex-wrap gap-5">
     <Chip variant="filled" label={type} color={type === "Work" && "bg-purple-700"} />
     <div class="flex flex-wrap gap-2">
       {#each tags as tag}
